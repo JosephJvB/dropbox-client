978203bcadb16cb4648e3a46dd0d315c6731e4bb
JosephJvB <joevanbo@pm.me>
2019-05-12 13:49:28 +1200

    add download endpoints

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const axios = require('axios');
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 const env = require('./env.json');
 // endpoints from https://www.dropbox.com/developers/documentation/http/documentation
 const api = require('./api.json');
 
 const Authorization = `Bearer ${env.app_token}`;
+
+module.exports = {
+  getFileMetaByName,
+  getFolderContents,
+  uploadLocalFile,
+  uploadRemoteFile,
+  downloadFile,
+  downloadImage
+};
 
 function getFileMetaByName (filePath) {;
   return axios(api.file_meta, {
@@ -63,17 +74,76 @@ function uploadRemoteFile (url, filePath) {
   });
 }
 
+// can download a file by path OR by ID
+function downloadFile (pathOrId, saveName) {
+  return axios(api.download, {
+    method: 'POST',
+    headers: {
+      Authorization,
+      'Content-Type': 'application/octet-stream',
+      'Dropbox-API-Arg': JSON.stringify({
+        path: pathOrId,
+      })
+    }
+  })
+  .then(({headers, data}) => {
+    // if no saveName: parse headers and save as dropbox fileName
+    const fileName = saveName || (getFileNameFromHeaders(headers));
+
+    const downloadPath = path.join(os.homedir(), 'Downloads', fileName);
+    fs.writeFileSync(downloadPath, data);
+
+    console.log('Saved file:', downloadPath);
+    return downloadPath;
+  });
+}
+
+// https://futurestud.io/tutorials/download-files-images-with-axios-in-node-js
+function downloadImage (pathOrId, saveName) {
+  return axios(api.download, {
+    method: 'POST',
+    responseType: 'stream',
+    headers: {
+      Authorization,
+      'Content-Type': 'application/octet-stream',
+      'Dropbox-API-Arg': JSON.stringify({
+        path: pathOrId,
+      })
+    }
+  })
+  .then(({data, headers}) => {
+    const fileName = saveName || (getFileNameFromHeaders(headers));
+
+    const downloadPath = path.join(os.homedir(), 'Downloads', fileName);
+    const writer = fs.createWriteStream(downloadPath);
+    writer.on('finish', () => console.log('Saved image:', downloadPath));
+    writer.on('error', e => console.error('WriteStream Error:', e));
+
+    data.pipe(writer);
+    return downloadPath;
+  });
+}
+
 //TESTED
-// log(getFileMetaByName('remooote.png'));
+// log(getFileMetaByName('haha-nice.txt.txt'));
 // log(getFolderContents(''));
 // log(uploadLocalFile('./test.txt', 'haha-nice.txt'));
 // log(uploadRemoteFile(
 //   'https://www.mixdownmag.com.au/sites/default/files/styles/flexslider_h400/public/images/Stu Main.jpg',
 //   'stewart.jpg'
 // ));
+// downloadFile('/haha-nice.txt.txt');
+// downloadImage('/remooote.png', 'new-name.png')
 //UNTESTED
+
 
 function log (p) {
   p.then(r => console.log(r.data))
   .catch(e => console.error(e));
+}
+
+function getFileNameFromHeaders (headers) {
+  const headersJson = JSON.parse(headers['dropbox-api-result']);
+  const headArr = headersJson.path_display.split('/');
+  return headArr[headArr.length - 1];
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 # Filessss
 
 home-brewed programmatic dropbox client.
+
+### DONE
+- Get single file meta-data
+- Get folder contents meta-data
+- Upload a single file from local machine
+- Upload a single file from url-resource
+- Download txtfile by dropbox-path or id
+- Download imagefile by dropbox-path or id
+
+### TODO
+- Refactor to one download function that can handle all file-types?
+- Batch upload/download?
+- Some kind of CLI. Pretty interface, spinners, selectors, scrollers and all that jazz.
