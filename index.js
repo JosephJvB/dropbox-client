091339cd3c26ae47770d7841014dc57e992e0cb0
JosephJvB <joevanbo@pm.me>
2019-05-17 14:28:00 +1200

    refactor: tokens

--- index.js
+++ index.js
@@ -7,51 +7,35 @@ const prompts = require('prompts');
 // hack to avoid loading ./lib before env.json exists, else error
 // see line above switch statement
 const loadLib = () => require('./lib');
-const envPath = path.join(__dirname, 'test-env.json');
-const deleteEnv = () => util.promisify(fs.unlink)(envPath);
+const {
+    checkTokenExists,
+    promptSetToken,
+    deleteToken
+} = require('./token');
 
 // make async to await token prompt
 (async function init() {
-    const envFileExists = fs.existsSync(envPath);
-    const tokenIsSet = () => {
-        return !!JSON.parse(
-            fs.readFileSync(envPath)
-        ).app_token;
-    }    
-    // set token if not exists
-    if(!envFileExists || !tokenIsSet()) {
-        await promptSetToken({required:true})
-        process.exit(0);
+
+    if(!await checkTokenExists()) {
+        // promptSetToken calls process.exit(0)
+        await promptSetToken({required: true});
     }
-    
+
     const helpText = `
         key: <> = required, [] = optional
 
         Usage:
         dbx <action> [options]
     
-        Actions:
-        auth: auth:set, auth:delete
-            dbx auth:set
-            dbx auth:delete
+        dbx connect
 
-        start-interactive: int, i
-            dbx i [path]
+        dbx token:set
+        dbx token:destroy
 
-        list-contents: list, ls
-            dbx list [path/id]
-    
-        meta: no alias
-            dbx meta <path/id>
-            -- cannot get meta-data at root path --
-    
-        upload: up
-            dbx up <local-file-path/url> [--as new-db-file.txt]
-    
-        download: down, dl
-            dbx download
+        dbx upload <local-file-path/url> [--as new-db-file.txt]
     `;
 
+    // flags start with '-', everything else a command
     const cmds = [];
     const flags = [];
     for(arg of process.argv.slice(2)) {
@@ -59,14 +43,9 @@ const deleteEnv = () => util.promisify(fs.unlink)(envPath);
         ? flags.push(arg)
         : cmds.push(arg)
     }
-    const [action, identifier] = cmds;
-    // flags
-    const asIdx = flags.findIndex(a => a === '--as');
-    const saveAsName = asIdx > 0 ? inputArgs[asIdx + 1] : null;
+    const [action] = cmds;
     const helpArgs = ['-h', 'help'];
     const help = flags.find(a => helpArgs.includes(a.toLowerCase())); 
-    const verboseArgs = ['-v', '--verbose'];
-    const verbose = flags.find(a => verboseArgs.includes(a.toLowerCase()));
 
     if(!action || help) {
         return console.log(helpText);
@@ -75,25 +54,11 @@ const deleteEnv = () => util.promisify(fs.unlink)(envPath);
     // hack here
     const lib = loadLib();
     switch(action.toLowerCase()) {
-        case 'meta': log(lib.getMetaData(identifier));
+        case 'connect': console.log('nice');
             break;
-        case 'ls':
-        case 'list':
-        case 'list-contents': log(lib.listContents(identifier, verbose));
+        case 'token:set': promptSetToken({required:false});
             break;
-        case 'up':
-        case 'upload': log(lib.handleUpload(identifier, saveAsName));
-            break;
-        case 'dl':
-        case 'down':
-        case 'download': log(lib.handleDownload());
-            break;
-        case 'auth':
-        case 'auth:set': promptSetToken({required:false});
-            break;
-        case 'auth:d':
-        case 'auth:destroy':
-        case 'auth:delete': deleteEnv();
+        case 'token:destroy': deleteToken();
             break;
         default: console.log(helpText);
     }
@@ -109,30 +74,4 @@ function log (p) {
         console.error('\n   dbx --help to see usage');
         process.exit(1);
     });
-}
-
-async function promptSetToken ({required}) {
-    const response = await prompts({
-        type: 'text',
-        name: 'token',
-        message: `${required ? 'No Authorization token set\n' : ''}Enter Dropbox Authorization token:\n`
-    }, {
-        onCancel: () => {
-            const url = 'https://www.dropbox.com/developers/apps?_tk=pilot_lp&_ad=topbar4&_camp=myapps';
-            if(required) {
-                console.log(`Must set token to proceed\nvisit ${url} to generate a token`);
-            }
-            process.exit(0);
-        }
-    });
-
-    // call replace in case user enters token WITH "Bearer "
-    // I only want the hash, I add "Bearer " at request time
-    // eg: ./lib/get-info line 5
-    // good or bad this way?? No idea but it's what im doing
-    const envJson = JSON.stringify({
-        app_token: response.token.replace('Bearer ', '')
-    },null,2);
-    fs.writeFileSync(envPath, envJson);
-    return;
 }
---
+++ token.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const prompts = require('prompts');
+
+const envPath = path.join(__dirname, 'test-env.json');
+
+exports.deleteToken = function deleteToken () {
+  return util.promisify(fs.unlink)(envPath);
+}
+
+exports.checkTokenExists = async function checkTokenExists () {
+  const envFileExists = fs.existsSync(envPath);
+  const tokenIsSet = () => {
+    return !!JSON.parse(
+        fs.readFileSync(envPath)
+    ).app_token;
+  }    
+
+  if(envFileExists && tokenIsSet()) {
+    return true
+  } else {
+    return false
+  }
+}
+
+exports.promptSetToken = async function promptSetToken ({required}) {
+  const response = await prompts({
+    type: 'text',
+    name: 'token',
+    message: `${required ? 'No app token set\n' : ''}Enter Dropbox app token:\n`
+  }, {
+    onCancel: () => {
+      if(required) {
+        const url = 'https://www.dropbox.com/developers/apps';
+        console.log(`Must set app-token to proceed\nVisit ${url} to generate a token`);
+      }
+      process.exit(0);
+    }
+  });
+
+  // call replace in case user enters token WITH "Bearer "
+  // I only want the hash, I add "Bearer " at request time
+  // eg: ./lib/get-info line 5
+  // good or bad this way?? No idea but it's what im doing
+  const envJson = JSON.stringify({
+      app_token: response.token.replace('Bearer ', '')
+  },null,2);
+  fs.writeFileSync(envPath, envJson);
+  
+  process.exit(0);
+}
