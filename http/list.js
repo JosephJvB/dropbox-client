7338e407239a4e297b95ec4e86e8f965511ac857
JosephJvB <joevanbo@pm.me>
2019-05-19 20:58:41 +1200

    WHOOPS

--- http/list.js
+++ http/list.js
@@ -7,8 +7,7 @@ module.exports = async function listContents (identifier = '') {
   const contents = await axios(api.folder_contents, {
       method: 'POST',
       headers: {
-          Authorization: "Bearer 34Qq9_UBBrAAAAAAAAAAGGmkimrEmOqeNwQuAbummdNSwjf-_7CvTY1kL4w9jVRA",
-          // Authorization: process.env.APP_TOKEN,
+          Authorization: process.env.APP_TOKEN,
           'Content-Type': 'application/json'
       },
       data: { path: identifier }
